ad4a96f2e03b0da57cebd5ec256d89837b22159a
dylan2766 <reevesdy@sheridancollege.ca>
2025-03-15 18:50:06 -0400

    - Completed lesson 6.3

--- Assets/Scripts/Hexagon.cs
+++ Assets/Scripts/Hexagon.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Hexagon : MonoBehaviour
 {
     SpriteRenderer sr;
+
+    public UnityEvent OnClick;
 
     void Start()
     {
@@ -14,5 +17,22 @@ public class Hexagon : MonoBehaviour
     public void ChangeColour()
     {
         sr.color = Random.ColorHSV();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (sr.bounds.Contains(mousePos))
+            {
+                OnClick.Invoke();
+            }
+        }
+    }
+
+    public void MakeMeBigger()
+    {
+        transform.localScale += Vector3.one * 0.1f;
     }
 }
--- Assets/Scripts/HexagonSpawner.cs
+++ Assets/Scripts/HexagonSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,8 @@ public class HexagonSpawner : MonoBehaviour
 {
     public GameObject prefab;
     public Button buttonToChangeColour;
+    public TextMeshProUGUI numberOfClicks;
+    int clicks = 0;
 
     public void Spawn()
     {
@@ -14,10 +17,18 @@ public class HexagonSpawner : MonoBehaviour
         Hexagon hexagon = newHexagon.GetComponent<Hexagon>();
 
         buttonToChangeColour.onClick.AddListener(hexagon.ChangeColour);
+
+        hexagon.OnClick.AddListener(AddToClickCounter);
     }
 
     public void StopListening()
     {
         buttonToChangeColour.onClick.RemoveAllListeners();
+    }
+
+    public void AddToClickCounter()
+    {
+        clicks++;
+        numberOfClicks.text = clicks.ToString();
     }
 }
