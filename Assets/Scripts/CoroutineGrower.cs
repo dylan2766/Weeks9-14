292c6c879b879418d2be15def35e670bb19c3dcd
dylan2766 <reevesdy@sheridancollege.ca>
2025-03-23 16:20:16 -0400

    - 7.3

--- Assets/Scripts/CoroutineGrower.cs
+++ Assets/Scripts/CoroutineGrower.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CoroutineGrower : MonoBehaviour
 {
     public AnimationCurve curve;
+    public AnimationCurve appleCurve;
     public float minSize = 0;
     public float maxSize = 1;
     public float t;
+    public Transform apple;
 
     public void StartGrowing()
     {
@@ -16,11 +18,20 @@ public class CoroutineGrower : MonoBehaviour
 
     public IEnumerator Grow()
     {
+        apple.localScale = Vector3.zero;
         t = 0;
         while (t < 1)
         {
             t += Time.deltaTime;
             transform.localScale = Vector3.one * maxSize * curve.Evaluate(t);
+            yield return null;
+        }
+
+        t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            apple.localScale = Vector3.one * maxSize * appleCurve.Evaluate(t);
             yield return null;
         }
     }
