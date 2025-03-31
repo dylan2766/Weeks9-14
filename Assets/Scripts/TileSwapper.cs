40ff9494f258eb32bea4c34be1761ec956a5a0aa
dylan2766 <reevesdy@sheridancollege.ca>
2025-03-31 12:35:45 -0400

    - Lesson 9.1 Completed

---
+++ Assets/Scripts/TileSwapper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSwapper : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public Tile grass;
+    public Tile stone;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3Int gridPos = tilemap.WorldToCell(mousePos);
+
+            Debug.Log(gridPos);
+
+            if(tilemap.GetTile(gridPos) == stone)
+            {
+                Debug.Log("This is stone, turn me into grass!");
+                tilemap.SetTile(gridPos, grass);
+            }
+            else
+            {
+                Debug.Log("This is grass, turn me into stone!");
+                tilemap.SetTile(gridPos, stone);
+            }
+        }
+    }
+}
